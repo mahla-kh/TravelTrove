2f398eb705d7fcda3c4fff4c705bbfc8a20cfe96
Mahla Khorashadi <mahla.kh09923531595@gmail.com>
2025-03-01 22:34:28 +0330

    adding vercle/speed-insights

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function App() {
   return (
